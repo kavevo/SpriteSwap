ad1419da879f886eaf487187d9ca14ec172cfa48
kavevo <casschillinger@gmail.com>
2026-08-27 13:35:11 -0500

    Add files via upload

---
+++ spriteswap/config.js
@@ -0,0 +1,6 @@
+// SpriteSwap Supabase configuration.
+// The anon/publishable key is intended for browser use. Database access is protected by RLS.
+window.SPRITESWAP_CONFIG = {
+  supabaseUrl: 'https://jnvjzyrwnoqlddwupkoi.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impudmp6eXJ3bm9xbGRkd3Vwa29pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NjkxMTAsImV4cCI6MjEwMzM0NTExMH0.07KY8wV85TDPyRU7gKQG3F4TJlzeEbZgXZVtbAe4bZ8'
+};
